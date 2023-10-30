7d8e91355117dbefcbd7184b341f3441af3e414f
LucasGonzalezSwd <lucas.ing.ind@gmail.com>
2023-10-30 15:22:44 -0300

    responsive home

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -36,13 +36,13 @@ function Login() {
   }
   return (
     <div className='flex flex-col  w-screen h-screen justify-center items-center'>
-        <h1 className='mb-[2rem] -mt-[2rem] text-xl'>
+        <h1 className='mb-[2rem] max-[453px]:mb-[2rem] max-[453px]:mt-[2rem] -mt-[2rem] text-xl'>
            INGRESAR AL PANEL DE ADMIN
         </h1>
-        <Link to="/"><button className='bg-gray-700 w-[3.5rem] h-[3.5rem] rounded-full absolute top-0 left-0 ml-[1.5rem] mt-[1rem]'>
+        <Link to="/"><button className='bg-gray-700  w-[3.5rem] h-[3.5rem] rounded-full absolute top-0 left-0 ml-[1.5rem] mt-[1rem]'>
         <FontAwesomeIcon icon={faArrowLeft} style={{color: "#fff",}} />
         </button></Link>
-        <div action="" className='flex flex-col justify-center items-center w-2/4 h-2/4 bg-gray-700 rounded-md' >
+        <div action="" className='flex max-[453px]:w-4/5  flex-col justify-center items-center w-2/4 h-2/4 bg-gray-700 rounded-md' >
       <label className='text-white' htmlFor="email">Usuario </label>
       <input
       className='rounded-sm mb-[1rem] pl-[0.1rem]'
